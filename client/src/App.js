1e440c416b00591ef5f23f4f5ce57358c34594ea
Pawel Godula <bolomaster7@gmail.com>
2023-05-28 12:48:03 +0200

    add 404 notfound page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/views/Footer/Footer';
 import Home from './components/pages/Home/Home';
 import NavBar from './components/views/Navbar/Navbar';
 import Register from './components/pages/Register/Register';
+import NotFound from './components/pages/NotFound/NotFoundPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path='/register' element={<Register />} />
+      <Route element={<NotFound />} />
     </Routes>
     <Footer />
   </Container>
